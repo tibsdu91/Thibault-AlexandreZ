22813c93798af8ca81b28882767d4ed23a8bcc3c
LourdSon <92031771+LourdSon@users.noreply.github.com>
2021-11-01 16:51:15 +0100

    Add files via upload

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chatbotclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/chatbotclojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns chatbotclojure.core-test
+  (:require [clojure.test :refer :all]
+            [chatbotclojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
